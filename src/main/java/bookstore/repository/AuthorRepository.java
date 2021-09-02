7250dc1f50b8383b9d9f038292d95a1dd816e199
lmgbsb <leandro.mesquita.galvao@gmail.com>
2021-09-02 18:12:51 -0300

    programei o método para incluir autores no banco de dados

--- src/main/java/bookstore/repository/AuthorRepository.java
+++ src/main/java/bookstore/repository/AuthorRepository.java
@@ -1,10 +1,35 @@
 package bookstore.repository;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import bookstore.model.Author;
 
 public class AuthorRepository {
+	
 
 	public void save(Author author) {
 		System.out.println("Salvando o autor " + author.getName());
+		String url = "jdbc:mysql://localhost:3306/bookstore";
+		String user = "bookstore_user";
+		String password = "senha";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = DriverManager.getConnection(url, user, password);
+			String saveAuthor = "insert into author(name, email, birth_date, mini_curriculum) "
+											+ "values (?, ?, ?, ?)";
+			PreparedStatement saveStatement = connection.prepareStatement(saveAuthor);
+			connection.setAutoCommit(false);
+			saveStatement.setString(1, author.getName());
+			saveStatement.setString(2, author.getEmail());
+			saveStatement.setDate(3, Date.valueOf(author.getBirthDate()));
+			saveStatement.setString(4, author.getMiniCurriculum());
+			saveStatement.execute();
+			connection.commit();
+		}catch(Exception e){
+			System.out.println(e);
+		}
 	}
 }
