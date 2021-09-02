0c6df93e7ee90e057130df70341e9b69e39e5ae5
lmgbsb <leandro.mesquita.galvao@gmail.com>
2021-09-02 15:07:01 -0300

    fiz a página de cadastro e programei o servlet para receber os dados de cadastro de autor

--- src/main/java/bookstore/controller/AuthorController.java
+++ src/main/java/bookstore/controller/AuthorController.java
@@ -1,7 +1,29 @@
 package bookstore.controller;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/author")
 public class AuthorController extends HttpServlet{
+
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String name = request.getParameter("name");
+		System.out.println(name);
+	}
+	
+	
+	
 
 }
